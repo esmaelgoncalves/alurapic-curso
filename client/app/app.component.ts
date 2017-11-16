a119fefa83473378e63179083104567d3b60cbc4
Esmael Gonçalves <esmael.goncalves@hotmail.com>
2017-11-16 21:29:54 -0200

    Desenvolvimento curso alura

---
+++ client/app/app.component.ts
@@ -0,0 +1,23 @@
+import {Component} from '@angular/core';
+import { Http } from '@angular/http';
+
+
+@Component({
+    selector: 'app',
+    templateUrl: './app/app.component.html'
+})
+export class AppComponent {
+
+    fotos : Object[] = [];
+
+    constructor(private http: Http){
+
+        http.get('v1/fotos')
+        .map(res => res.json())
+        .subscribe(
+            fotos => this.fotos = fotos,
+            erro => console.log(erro)
+        );  
+    }
+} 
+
---
+++ client/app/app.module.ts
@@ -0,0 +1,19 @@
+import { FotoModule } from './foto/foto.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http/';
+
+import 'rxjs/add/operator/map';
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FotoModule,
+    HttpModule
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
